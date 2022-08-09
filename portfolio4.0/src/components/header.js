4f37ad8f66c65a4de24d78f6799ff79628b81259
phellweg <peter.hellwege@vizientinc.com>
2022-08-09 10:00:55 -0600

    adding basic functionality

--- portfolio4.0/src/components/header.js
+++ portfolio4.0/src/components/header.js
@@ -1,4 +1,7 @@
 import './header.css'
+import {Link} from 'react-router-dom';
+
+
 
 export default () => {
 
@@ -6,14 +9,14 @@ export default () => {
         <>
             <div class='headWrap'>
                 <div class='headLeft'>
-                    <a href=''>Peter Hellwege</a>
+                    <Link to='/#AboutMeSection'>Peter Hellwege</Link>
                 </div>
                 <div class='headRight'>
                     <ul>
                         <li>list item</li>
-                        <li>list item</li>
-                        <li>list item</li>
-                        <li>list item</li>
+                        <li>github</li>
+                        <li>linkedin</li>
+                        <li>hamburger menu</li>
                     </ul>
                 </div>
             </div>
--- portfolio4.0/src/components/section3.js
+++ portfolio4.0/src/components/section3.js
@@ -3,7 +3,7 @@ import './section.css'
 export default () => {
 
     return (
-        <div class='sectionWrap'>
+        <div class='sectionWrap' id='AboutMeSection'>
             <div class='section3Left'>
                 <h1>About Me</h1>
                 <h3>Things I like</h3>
