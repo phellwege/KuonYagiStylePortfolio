e5c17f7a4ca7e0f0919425eebb28ad58e777659d
phellweg <peter.hellwege@vizientinc.com>
2022-08-09 13:12:09 -0600

    setting up parallax links

--- portfolio4.0/src/views/home.js
+++ portfolio4.0/src/views/home.js
@@ -1,5 +1,7 @@
+import React, { useEffect, useRef } from "react";
 import './home.css';
 import { Parallax, ParallaxLayer } from '@react-spring/parallax'
+import {Link} from 'react-router-dom';
 import Header from '../components/header';
 import Section1 from '../components/section1';
 import Section2 from '../components/section2';
@@ -7,11 +9,39 @@ import Section3 from '../components/section3';
 import Section4 from '../components/section4';
 
 export default () => {
-
+    const ref = useRef()
+    
+        
     return (
         
         <div className='homeWrapper'>
             <Header/>
+            {/* <div id="fp-nav" class="left">
+                <ul><Parallax ref={ref}>
+                    <li>
+                        
+                        <Link to={ref.current.scrollTo(0)} class="">
+                            <span></span>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to={ref.current.scrollTo(1)} class="active">
+                            <span></span>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to={ref.current.scrollTo(2)} class="">
+                            <span></span>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to={ref.current.scrollTo(3)} class="">
+                            <span></span>
+                        </Link>
+                    </li>
+                    </Parallax>
+                </ul>
+            </div> */}
                 <Parallax pages={4} style={{ top: '0', left: '0' }}>
                     <ParallaxLayer
                         offset={0}
@@ -19,7 +49,7 @@ export default () => {
                         style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
                         <Section1/>
                     </ParallaxLayer>
-                <ParallaxLayer offset={1} speed={1} style={{ backgroundColor: '#ff6d6d' }} />
+                <ParallaxLayer offset={1} speed={1} />
                     <ParallaxLayer
                         offset={1}
                         speed={0.5}
@@ -27,11 +57,10 @@ export default () => {
                         display: 'flex',
                         justifyContent: 'center',
                         alignItems: 'center',
-                        color: 'white',
                         }}>
                         <Section2/>
                     </ParallaxLayer>
-                <ParallaxLayer offset={2} speed={1} style={{ backgroundColor: 'white' }} />
+                <ParallaxLayer offset={2} speed={1} />
                     <ParallaxLayer
                         offset={2}
                         speed={0.5}
@@ -42,7 +71,7 @@ export default () => {
                         }}>
                         <Section3/>
                     </ParallaxLayer>
-                <ParallaxLayer offset={3} speed={1} style={{ backgroundColor: '#ff6d6d' }} />
+                <ParallaxLayer offset={3} speed={1} />
                     <ParallaxLayer
                         offset={3}
                         speed={0.5}
@@ -50,7 +79,6 @@ export default () => {
                         display: 'flex',
                         justifyContent: 'center',
                         alignItems: 'center',
-                        color: 'white',
                         }}>
                         <Section4/>
                     </ParallaxLayer>
