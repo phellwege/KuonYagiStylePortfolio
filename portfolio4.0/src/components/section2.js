21f893895028c9235e0321edfd94f53fe9802a34
phellweg <peter.hellwege@vizientinc.com>
2022-08-09 10:03:21 -0600

    expanded basic functionality

--- portfolio4.0/src/components/section2.js
+++ portfolio4.0/src/components/section2.js
@@ -6,7 +6,7 @@ export default () => {
         <div class='sectionWrap'>
             <div class='section2Left'>
                 <h1>Projects</h1>
-                <button/>
+                <button>View Projects</button>
             </div>
             <div class='section2Right'>
                 <h1>img</h1>
