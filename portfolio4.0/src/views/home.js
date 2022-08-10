a6db0ff768e914c69aafb9ba415db543bf87e3f1
phellweg <peter.hellwege@vizientinc.com>
2022-08-10 14:31:41 -0600

    added page scroll feature

--- portfolio4.0/src/views/home.js
+++ portfolio4.0/src/views/home.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useRef } from "react";
 import './home.css';
-import { Parallax, ParallaxLayer } from '@react-spring/parallax';
-// import ReactFullpage from "@fullpage/react-fullpage";
+import Fullpage, { FullPageSections, FullpageSection, FullpageNavigation } from '@ap.cx/react-fullpage';
 import Header from '../components/header';
 import Section1 from '../components/section1';
 import Section2 from '../components/section2';
@@ -9,59 +8,36 @@ import Section3 from '../components/section3';
 import Section4 from '../components/section4';
 
 export default () => {
-    const scrollToSection = (props) => {
-        // not sure how to write the routing yet
-    }
+
+        const SectionStyle ={
+            height: '100vh',
+            width: '100%',
+            display: 'flex', 
+            justifyContent: 'center',
+            alignItems: 'center'
+        };
+
     return (
         <div className='homeWrapper'>
             <Header/>
-                {/* <Section1 id='section1'/> */}
-                {/* default active selection section1. */}
-                {/* <Section2 id='section2'/>
-                <Section3 id='section3'/>
-                <Section4 id='section4'/> */}
 
-                <Parallax pages={2.5} style={{ top: '0', left: '0' }}>
-                    <ParallaxLayer
-                        offset={0}
-                        speed={0.5}
-                        style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                        <Section1/>
-                    </ParallaxLayer>
-                <ParallaxLayer offset={1} speed={1} style={{ backroundColor: '#ff6d6d'}}/>
-                    <ParallaxLayer
-                        offset={.75}
-                        speed={0.5}
-                        style={{
-                        display: 'flex',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        }}>
-                        <Section2/>
-                    </ParallaxLayer>
-                <ParallaxLayer offset={2} speed={1} />
-                    <ParallaxLayer
-                        offset={1}
-                        speed={0.5}
-                        style={{
-                        display: 'flex',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        }}>
-                        <Section3/>
-                    </ParallaxLayer>
-                <ParallaxLayer offset={3} speed={1} />
-                    <ParallaxLayer
-                        offset={1.75}
-                        speed={0.5}
-                        style={{
-                        display: 'flex',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        }}>
-                        <Section4/>
-                    </ParallaxLayer>
-                </Parallax>
+                <Fullpage>
+                    <FullpageNavigation/>
+                    <FullPageSections>
+                        <FullpageSection style={{SectionStyle}}>
+                            <Section1/>
+                        </FullpageSection>
+                        <FullpageSection style={{SectionStyle}}>
+                            <Section2/>
+                        </FullpageSection>
+                        <FullpageSection style={{SectionStyle}}>
+                            <Section3/>
+                        </FullpageSection>
+                        <FullpageSection style={{SectionStyle}}>
+                            <Section4/>
+                        </FullpageSection>
+                    </FullPageSections>
+                </Fullpage>
 
         </div>
     )
