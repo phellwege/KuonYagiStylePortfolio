102914c1006f3e9f7c5d2528668b4df1676465ad
phellweg <peter.hellwege@vizientinc.com>
2022-08-10 12:41:30 -0600

    removed fullpage in favor of manually making it

--- portfolio4.0/src/views/home.js
+++ portfolio4.0/src/views/home.js
@@ -2,7 +2,6 @@ import React, { useEffect, useRef } from "react";
 import './home.css';
 import { Parallax, ParallaxLayer } from '@react-spring/parallax';
 import ReactFullpage from "@fullpage/react-fullpage";
-import "./styles.css";
 import Header from '../components/header';
 import Section1 from '../components/section1';
 import Section2 from '../components/section2';
@@ -10,24 +9,15 @@ import Section3 from '../components/section3';
 import Section4 from '../components/section4';
 
 export default () => {
-    const SEL = "custom-section";
-    const SECTION_SEL = `.${SEL}`;
-    const originalColors = [
-        "#ff5f45",
-        "#0798ec",
-        "#fc6c7c",
-        "#435b71",
-        "orange",
-        "blue",
-        "purple",
-        "yellow"
-    ];
-
     
     return (
-        
         <div className='homeWrapper'>
             <Header/>
+                <Section1/>
+                <Section2/>
+                <Section3/>
+                <Section4/>
+                
                 {/* <Parallax pages={2.5} style={{ top: '0', left: '0' }}>
                     <ParallaxLayer
                         offset={0}
@@ -35,7 +25,7 @@ export default () => {
                         style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
                         <Section1/>
                     </ParallaxLayer>
-                <ParallaxLayer offset={1} speed={1} />
+                <ParallaxLayer offset={1} speed={1} style={{ backroundColor: '#ff6d6d'}}/>
                     <ParallaxLayer
                         offset={.75}
                         speed={0.5}
