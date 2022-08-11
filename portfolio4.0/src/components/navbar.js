0e3c742a40be64e6b917b0be2c80cf9cb8b2a0fd
phellweg <peter.hellwege@vizientinc.com>
2022-08-10 21:43:12 -0600

    fixed hamburgers

--- portfolio4.0/src/components/navbar.js
+++ portfolio4.0/src/components/navbar.js
@@ -15,22 +15,20 @@ export default (props) => {
     }
     return (
         <div id='Hamburger_Menu'>
-            
                 <nav className="navBar">
-                        <button onClick={handleToggle}>{navbarOpen ? (
-                            <MdClose style={{ color: "#7b7b7b", width: "40px", height: "40px" }} />
-                            ) : (
-                            <FiMenu style={{ color: "#7b7b7b", width: "40px", height: "40px" }} />
-                            )}
-                        </button>
-                            <ul className={`menuNav ${navbarOpen ? " showMenu" : ""}`}>
-                                <li><Link to="/#Section1" onClick={() => closeMenu()}>Home</Link></li>
-                                <li><Link to="/#Section2" onClick={() => closeMenu()}>Section2</Link></li>
-                                <li><Link to="/#Section3" onClick={() => closeMenu()}>Section3</Link></li>
-                                <li><Link to="/#Section4" onClick={() => closeMenu()}>Section4</Link></li>
-                            </ul>
-                    </nav>
-            
+                    <button onClick={handleToggle}>{navbarOpen ? (
+                        <MdClose style={{ color: "#7b7b7b", width: "40px", height: "40px" }} />
+                        ) : (
+                        <FiMenu style={{ color: "#7b7b7b", width: "40px", height: "40px" }} />
+                        )}
+                    </button>
+                        <ul className={`menuNav ${navbarOpen ? " showMenu" : ""}`}>
+                            <li><Link to="/#Section1" onClick={() => closeMenu()}>Home</Link></li>
+                            <li><Link to="/#Section2" onClick={() => closeMenu()}>Section2</Link></li>
+                            <li><Link to="/#Section3" onClick={() => closeMenu()}>Section3</Link></li>
+                            <li><Link to="/#Section4" onClick={() => closeMenu()}>Section4</Link></li>
+                        </ul>
+                </nav>
         </div>
     )
 }
