f9b7a3f117bd1e7c451943674fe4e43828414cec
phellweg <peter.hellwege@vizientinc.com>
2022-08-11 14:45:54 -0600

    added project page skeleton

--- portfolio4.0/src/views/aboutMe.js
+++ portfolio4.0/src/views/aboutMe.js
@@ -1,4 +1,5 @@
 import Header from '../components/header';
+import './aboutMe.css';
 
 export default () => {
 
--- portfolio4.0/src/views/projectsPage.js
+++ portfolio4.0/src/views/projectsPage.js
@@ -1,18 +1,106 @@
 import Header from '../components/header';
+import './projectPage.css';
+
+import Fullpage, { FullPageSections, FullpageSection, FullpageNavigation } from '@ap.cx/react-fullpage';
 
 export default () => {
 
+    const SectionStyle ={
+        height: '100vh',
+        width: '100%',
+        display: 'flex', 
+        justifyContent: 'center',
+        alignItems: 'center'
+    };
+
     return (
         <>
-            <Header/>
-            <div className='projectWrap'>
-                <div className='projectLeft'>
-                    <p>Images Here</p>
-                </div>
-                <div className='projectRight'>
-                    <p>description</p>
-                </div>
-            </div>
+        <Header/>
+            <Fullpage>
+                    <FullpageNavigation/>
+                    <FullPageSections>
+                        <FullpageSection style={{SectionStyle}}>
+                            <div className='projectWrap'>
+                                <div className='projectLeft'>
+                                    <p>Images Here</p>
+                                </div>
+                                <div className='projectRight'>
+                                    <p>description</p>
+                                </div>
+                            </div>
+                        </FullpageSection>
+                        <FullpageSection style={{SectionStyle}}>
+                            <div className='projectWrap'>
+                                <div className='projectLeft'>
+                                    <p>Images Here</p>
+                                </div>
+                                <div className='projectRight'>
+                                    <p>description</p>
+                                </div>
+                            </div>
+                        </FullpageSection>
+                        <FullpageSection style={{SectionStyle}}>
+                            <div className='projectWrap'>
+                                <div className='projectLeft'>
+                                    <p>Images Here</p>
+                                </div>
+                                <div className='projectRight'>
+                                    <p>description</p>
+                                </div>
+                            </div>
+                        </FullpageSection>
+                        <FullpageSection style={{SectionStyle}}>
+                            <div className='projectWrap'>
+                                <div className='projectLeft'>
+                                    <p>Images Here</p>
+                                </div>
+                                <div className='projectRight'>
+                                    <p>description</p>
+                                </div>
+                            </div>
+                        </FullpageSection>
+                        <FullpageSection style={{SectionStyle}}>
+                            <div className='projectWrap'>
+                                <div className='projectLeft'>
+                                    <p>Images Here</p>
+                                </div>
+                                <div className='projectRight'>
+                                    <p>description</p>
+                                </div>
+                            </div>
+                        </FullpageSection>
+                        <FullpageSection style={{SectionStyle}}>
+                            <div className='projectWrap'>
+                                <div className='projectLeft'>
+                                    <p>Images Here</p>
+                                </div>
+                                <div className='projectRight'>
+                                    <p>description</p>
+                                </div>
+                            </div>
+                        </FullpageSection>
+                        <FullpageSection style={{SectionStyle}}>
+                            <div className='projectWrap'>
+                                <div className='projectLeft'>
+                                    <p>Images Here</p>
+                                </div>
+                                <div className='projectRight'>
+                                    <p>description</p>
+                                </div>
+                            </div>
+                        </FullpageSection>
+                        <FullpageSection style={{SectionStyle}}>
+                            <div className='projectWrap'>
+                                <div className='projectLeft'>
+                                    <p>Images Here</p>
+                                </div>
+                                <div className='projectRight'>
+                                    <p>description</p>
+                                </div>
+                            </div>
+                        </FullpageSection>
+                    </FullPageSections>
+                </Fullpage>
         </>
     )
 }
