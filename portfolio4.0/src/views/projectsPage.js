6b6aae99af256d4209f2ba5dfcaf7b9487e0ff94
phellweg <peter.hellwege@vizientinc.com>
2022-08-11 13:33:29 -0600

    added basic skeleton for project page

--- portfolio4.0/src/views/projectsPage.js
+++ portfolio4.0/src/views/projectsPage.js
@@ -5,7 +5,14 @@ export default () => {
     return (
         <>
             <Header/>
-
+            <div className='projectWrap'>
+                <div className='projectLeft'>
+                    <p>Images Here</p>
+                </div>
+                <div className='projectRight'>
+                    <p>description</p>
+                </div>
+            </div>
         </>
     )
 }
