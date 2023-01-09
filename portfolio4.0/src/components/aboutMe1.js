9df55b3e10dca099f4b959d84a2b003d54cca366
Peter <phellwege1@gmail.com>
2023-01-09 16:49:11 -0700

    fixed typo

--- portfolio4.0/src/components/aboutMe1.js
+++ portfolio4.0/src/components/aboutMe1.js
@@ -17,7 +17,7 @@ export default (props) => {
                         <img src={Headshot} alt='headshot img' id='headshot'/>
                         <h3>A Bit About Me</h3>
                         <p>
-                            In my spare time I enjoy art, fishing, riding my bike, or just sitting quietly and reading a book.
+                            In my spare time I enjoy art, fishing, riding my bike, or just reading a book.
                             I'm located in scenic Boise ID, but love to travel.
                         </p>
                 </div>
